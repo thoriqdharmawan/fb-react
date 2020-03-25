03ebb883fc3f6e8a3402c1fdd1e9fb5284380fb6
thoriqdharmawan <thoriqd21@gmail.com>
2020-03-25 15:43:56 +0700

    Updates

---
+++ precache-manifest.d2b45a10f7adfa4652d7e2a41dc3d1d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058ed56796db13e1d82ac5766c798843",
+    "url": "/fb-react/index.html"
+  },
+  {
+    "revision": "a6723f3aec0148d91205",
+    "url": "/fb-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b78902397aab4d176e1",
+    "url": "/fb-react/static/js/2.55d7c7cb.chunk.js"
+  },
+  {
+    "revision": "bad6e083d4d2a996fe60e513745a4089",
+    "url": "/fb-react/static/js/2.55d7c7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6723f3aec0148d91205",
+    "url": "/fb-react/static/js/main.8c2a7ae9.chunk.js"
+  },
+  {
+    "revision": "95fbbd91f89e2b4066c7",
+    "url": "/fb-react/static/js/runtime-main.8365441a.js"
+  }
+]);
